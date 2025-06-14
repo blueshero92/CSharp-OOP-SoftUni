a79112426b572810778d40c3071fc43a16b80003
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-14 23:20:29 +0300

    Add files via upload

---
+++ 01. Inheritance Lab/01, 02, Single and Multiple Inheritance/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new();
+
+            puppy.Bark();
+            puppy.Eat();
+            puppy.Weep();
+        }
+    }
+}
