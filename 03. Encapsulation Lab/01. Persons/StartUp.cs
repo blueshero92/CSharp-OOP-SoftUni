02795d5438bbd501cedf436d8b62fb8c645ec484
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:25:59 +0300

    Add files via upload

---
+++ 03. Encapsulation Lab/01. Persons/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            List<Person> people = new();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new(tokens[0], tokens[1], int.Parse(tokens[2]));
+
+                people.Add(person);
+            }
+
+            foreach (Person person in people.OrderBy(p => p.FirstName).ThenBy(p => p.Age))
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
