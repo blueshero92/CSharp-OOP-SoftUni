11b0acbc383bc3d68a4e1b931675d11f71aa639d
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:08:10 +0300

    Add files via upload

---
+++ 01. Inheritance Lab/04. Random List/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+
+            randomList.Add("Dido");
+            randomList.Add("Pesho");
+            randomList.Add("Gosho");
+
+           Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
