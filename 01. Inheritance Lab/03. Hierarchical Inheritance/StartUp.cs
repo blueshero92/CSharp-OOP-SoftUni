2083d86c159bbe8fd681eaa15d868456ff6b2f36
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:05:29 +0300

    Add files via upload

---
+++ 01. Inheritance Lab/03. Hierarchical Inheritance/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new();
+            Dog dog = new Dog();
+
+            dog.Bark();
+            cat.Eat();
+            dog.Eat();
+            cat.Meow();
+        }
+    }
+}
