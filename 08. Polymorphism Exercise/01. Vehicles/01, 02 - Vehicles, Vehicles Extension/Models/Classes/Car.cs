540909ed9f60dbba2fddc0acd789d5f9aa4ce8d8
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 16:42:18 +0300

    Add files via upload

---
+++ 08. Polymorphism Exercise/01. Vehicles/01, 02 - Vehicles, Vehicles Extension/Models/Classes/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles.Models.Interfaces;
+
+namespace Vehicles.Models.Classes
+{
+    public class Car : Vehicle
+    {
+        private const double FuelConsumptionIncrease = 0.9;
+
+        public Car(double fuelQuantity, double fuelConsumption, double tankCapacity)
+            : base(fuelQuantity, fuelConsumption, tankCapacity, FuelConsumptionIncrease)
+        {
+        }
+    }
+}
---
+++ 08. Polymorphism Exercise/01. Vehicles/01, 02 - Vehicles, Vehicles Extension/Models/Classes/Truck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles.Models.Interfaces;
+
+namespace Vehicles.Models.Classes
+{
+    public class Truck : Vehicle
+    {
+        private const double FuelConsumptionIncrease = 1.6;
+        public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity)
+            : base(fuelQuantity, fuelConsumption, tankCapacity, FuelConsumptionIncrease)
+        {
+        }
+
+
+        public override void Refuel(double fuel)
+        {
+            if (this.TankCapacity < fuel + this.FuelQuantity)
+            {
+                throw new ArgumentException($"Cannot fit {fuel} fuel in the tank");
+            }
+
+            base.Refuel(fuel * 0.95);
+
+        }
+    }
+}
