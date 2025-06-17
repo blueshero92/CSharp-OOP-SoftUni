db7b6b2e4632c0644858b74367785019be9707fd
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:15:40 +0300

    Add files via upload

---
+++ 02. Inheritance Exercise/02. Zoo/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public abstract class Animal
+    {
+        private string name;
+
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+    }
+}
