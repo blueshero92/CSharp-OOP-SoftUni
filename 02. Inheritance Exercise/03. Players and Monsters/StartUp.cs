b53eccf10baf981a7d171f8ad9dc4f328b01ec2e
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:17:54 +0300

    Add files via upload

---
+++ 02. Inheritance Exercise/03. Players and Monsters/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DarkWizard wizard = new DarkWizard("Gandalf", 6);
+
+            Console.WriteLine(wizard);
+        }
+    }
+}
