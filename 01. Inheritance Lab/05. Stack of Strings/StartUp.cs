e62f82ab6b067bb306a4ef164a9a2cdfc80e8e0a
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:10:16 +0300

    Add files via upload

---
+++ 01. Inheritance Lab/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            List<string> strings = new List<string>() { "Hi", "Bye", "Shit" };          
+
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.AddRange(strings);
+
+            Console.WriteLine(string.Join(" ", stack));
+
+        }
+    }
+}
