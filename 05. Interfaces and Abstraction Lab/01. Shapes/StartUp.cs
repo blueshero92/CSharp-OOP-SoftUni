ce8e6837e54b6df78de71b6a3cedce455a2e7c25
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:57:43 +0300

    Add files via upload

---
+++ 05. Interfaces and Abstraction Lab/01. Shapes/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IDrawable circle = new Circle(3);
+            IDrawable rectangle = new Rectangle(4, 5);
+
+            circle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
