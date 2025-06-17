190c67f9255069c01981dfeaff5bddb323575247
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:21:15 +0300

    Add files via upload

---
+++ 02. Inheritance Exercise/04. Need for Speed/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private int horsePower;
+        private double fuel;
+        private const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public int HorsePower
+        {
+            get { return this.horsePower; }
+            set { this.horsePower = value; }
+        }
+
+        public double Fuel
+        {
+            get { return this.fuel; }
+            set { this.fuel = value; }
+        }
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public void Drive(double kilometers)
+        {
+            Fuel -= kilometers * FuelConsumption;
+        }
+        
+
+    }
+}
