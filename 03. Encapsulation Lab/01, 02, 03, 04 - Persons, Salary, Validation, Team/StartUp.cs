c526f860827f6ef5946d0334fa018ae93a6e0d3f
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:32:20 +0300

    Add files via upload

---
+++ 03. Encapsulation Lab/01, 02, 03, 04 - Persons, Salary, Validation, Team/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            
+            Team team = new("SoftUni");
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new(tokens[0], tokens[1], int.Parse(tokens[2]), decimal.Parse(tokens[3]));
+
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+
+        }
+    }
+}
---
+++ 03. Encapsulation Lab/01, 02, 03, 04 - Persons, Salary, Validation, Team/Team.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+
+        }
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+        public IReadOnlyCollection <Person> FirstTeam => firstTeam;
+        public IReadOnlyCollection <Person> ReserveTeam => reserveTeam;
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.firstTeam.Add(person);
+            }
+            else
+            {
+                this.reserveTeam.Add(person);
+            }
+        }
+
+    }
+}
