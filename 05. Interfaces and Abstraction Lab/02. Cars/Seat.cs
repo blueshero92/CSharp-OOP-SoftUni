5dac756defbc0ab67fe03d44f886e5e0d470040b
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 16:00:38 +0300

    Add files via upload

---
+++ 05. Interfaces and Abstraction Lab/02. Cars/Seat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string model;
+        private string color;
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {string.Format(nameof(Seat))} {Model}";
+        }
+    }
+}
---
+++ 05. Interfaces and Abstraction Lab/02. Cars/Tesla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        private string model;
+        private string color;
+        private int battery;
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {string.Format(nameof(Tesla))} {Model} with {Battery} Batteries";
+        }
+    }
+}
