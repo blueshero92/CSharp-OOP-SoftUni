a66a390622f5bc021b320dcb3f281537ec95688f
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 15:13:44 +0300

    Add files via upload

---
+++ 02. Inheritance Exercise/01. Person/Child.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, uint age) : base(name, age)
+        {
+        }
+
+        public override uint Age
+        {
+            get { return base.Age; }
+
+            set { base.Age = value; }
+        }
+    }
+}
---
+++ 02. Inheritance Exercise/01. Person/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private uint age;
+
+        public Person(string name, uint age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; } 
+            set { this.name = value; }
+        }
+
+        public virtual uint Age
+        {
+            get { return this.age; } 
+            set { this.age = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
