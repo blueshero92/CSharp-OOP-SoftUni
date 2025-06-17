ddcda332eba510ed85a42f56e215e8adbec73f64
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 16:37:35 +0300

    Add files via upload

---
+++ 07. Polymorphism Lab/03. Shapes/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace Shapes
+{
+   public class StartUp
+    {
+        static void Main()
+        {
+            Rectangle rectangle = new(2, 4);
+            Circle circle = new(6);
+
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.Draw());
+
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
