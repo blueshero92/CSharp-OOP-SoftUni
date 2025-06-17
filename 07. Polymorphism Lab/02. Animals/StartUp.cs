f8628b29235f8e74a5deaa4a732acc8ad31a6276
blueshero92 <133801686+blueshero92@users.noreply.github.com>
2025-06-17 16:35:38 +0300

    Add files via upload

---
+++ 07. Polymorphism Lab/02. Animals/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Animals
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Animal cat = new Cat("Sivcho", "Whiskas");
+            Animal dog = new Dog("Gogo", "Konserva");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+
+        }
+    }
+}
